e8a92782c5014b80e9afaee713fea340e1828ab2
Vivek Shankar <shankar9@buffalo.edu>
2021-10-16 16:01:41 -0400

    Ransac initial commit

--- src/ransac.cpp
+++ src/ransac.cpp
@@ -1,2 +1,49 @@
-#include <stdio.h>
-#include <ros/std_msgs.h>
+#include <ros/ros.h>
+
+#include "ackermann_msgs/AckermannDriveStamped.h"
+
+#include "ackermann_msgs/AckermannDrive.h"
+
+#include "nav_msgs/Odometry.h"
+
+#include "sensor_msgs/LaserScan.h"
+
+// for printing
+#include <iostream>
+
+// for RAND_MAX
+#include <cstdlib>
+
+class Ransac {
+private:
+    ros::NodeHandle n;
+
+    double max_speed, max_steering_angle;
+
+    double safe_distance_threshold = 1;
+
+    // Listen for odom & laser scan messages
+    ros::Subscriber scan_sub;
+
+    // Publish drive data
+    ros::Publisher drive_pub;
+
+    // previous desired steering angle
+    double prev_angle=0.0;
+
+    bool first_run = true;
+
+    int64_t turn = 0;
+
+public:
+    Ransac() {
+        n = ros::NodeHandle("~");
+    }
+};
+
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "ransac");
+    Ransac ransac;
+    ros::spin();
+    return 0;
+}
