0691e4db82914b2f09c344ad2cc1f6819d0a9ec8
Vivek Shankar <shankar9@buffalo.edu>
2021-10-20 18:23:58 +0000

    Fixed build & able to visualise detected point and added code to detect atleast one line from the detected points

--- src/ransac.cpp
+++ src/ransac.cpp
@@ -29,11 +29,11 @@ private:
 
     bool first_run = true;
 
-    double inlier_threshold = 0.3;
+    double inlier_threshold = 0.5;
 
     struct Point {
-        int x;
-        int y;
+        double x;
+        double y;
     };
 
 public:
@@ -60,8 +60,8 @@ public:
         line_strip_marker.type = visualization_msgs::Marker::LINE_STRIP;
         line_list_marker.type = visualization_msgs::Marker::LINE_LIST;
         points_marker.header.frame_id = "base_link";
-        points_marker.scale.x = 0.1;
-        points_marker.scale.y = 0.1;
+        points_marker.scale.x = 0.01;
+        points_marker.scale.y = 0.01;
         points_marker.scale.z = 0;
         points_marker.color.a = 1.0;
         points_marker.color.r = 0.0;
@@ -69,7 +69,10 @@ public:
         points_marker.color.b = 0.0;
 
         // Find the cartesian coordinates of points from the scan data.
-        for (auto i : ranges) {
+        for (int i = 0; i < ranges.size(); i++) {
+            // Ignore points at max scan range
+            if(ranges[i] == 3.0)
+                continue;
             theta = (angle_increment * i) + angle_min;
 
             p.x = ranges[i] * cos (theta);
@@ -79,12 +82,12 @@ public:
         }
 
         std::vector<geometry_msgs::Point> viz_points = processToSend(points);
-        ROS_INFO_STREAM("viz points = "<<viz_points[200].x<<" "<<viz_points[200].y<<" "<<viz_points[200].z);
-	points_marker.points = viz_points;
-        marker_pub.publish(points_marker);
+        // ROS_INFO_STREAM("viz points = "<<viz_points[200].x<<" "<<viz_points[200].y<<" "<<viz_points[200].z);
+	    points_marker.points = viz_points;
+         marker_pub.publish(points_marker);
 
         // Run ransac on the points we got above to detect lines.
-        // std::vector<Point> line = ransac_cal(points);
+        std::vector<std::pair<Point, Point>> line = ransac_cal(points);
 
         /* Publish the selected lines to rviz, if we found more than one line we should check if
          * they intersect, if they do we should publish the non-intersecting end points and the intersection point
@@ -107,102 +110,104 @@ public:
         }
         return points;
     }
-//
-//    std::vector <std::pair<Point, Point>> ransac_cal(std::vector<Point> points) {
-//
-//        int points_size = points.size();
-//        int iterations = (int) (points_size * 0.1);
-//        std::pair <Point, Point> best_candidate_pair;
-//        std::vector <std::pair<Point, Point>> best_candidates;
-//        double best_candidate_inliers_count = 0;
-//
-//        // Unless we have discarded 95% of the points keep going
-//        while (points.size() > (int) (0.05 * points_size)) {
-//            // In one iteration of this loop have detected one line
-//            while (iterations--) {
-//                // Random number between zero and one
-//                double random1 = ((double) rand() / RAND_MAX);
-//                random1 *= points.size();
-//                double random2 = ((double) rand() / RAND_MAX);
-//                random2 *= points.size();
-//
-//                std::vector <Point> inliers, outliers;
-//
-//                Point point_a = points[(int) random1];
-//                // points.erase(points.begin() + (int) random1);
-//                Point point_b = points[(int) random2];
-//                // points.erase(points.begin() + (int) random1);
-//
-//                // First - A point, Second - Distance of "First" point from the line formed by point_a & point_b
-//                std::vector<std::pair<Point ,double>> distances;
-//
-//                for (int i = 0; i < points.size(); i++) {
-//                    if (i == (int) random1 || i == (int) random2)
-//                        continue;
-//
-//                    // Calculate distance of points from the line formed by point_a & point_b
-//                    distances.push_back(calculate_dist(point_a, point_b, points[i]));
-//                }
-//
-//                /*
-//                 * Now from the distance vector populate inliers and outliers but comparing with the threshold,
-//                 * if the number of outliers is max amongst the ones detected so far this is the best candidate.
-//                 * */
-//                int inliers_count = 0;
-//                for (auto i: distances) {
-//                    if (i.second < inlier_threshold) {
-//                        inliers_count++;
-//                        // inliers.push_back(i);
-//                    } else {
-//                        outliers.push_back(i.first);
-//                    }
-//                }
-//
-//                if (inliers_count > best_candidate_inliers_count) {
-//                    // Discard the inliers
-//                    points = intersection(points, outliers);
-//                    best_candidate_pair.first = point_a;
-//                    best_candidate_pair.second = point_b;
-//                    best_candidate_inliers_count = inliers.size();
-//                }
-//            }
-//        }
-//    }
-//
-//    // Calculate distance of point_c from the line being formed by point_a & point_b
-//    // Adapted from the formula & https://stackoverflow.com/a/12132746
-//    std::pair<Point ,double> calculate_dist(Point point_a, Point point_b, Point point_c) {
-//        double a, b, c;
-//        getLine (point_a, point_b, a, b, c);
-//        return std::pair<Point ,double>(point_c,abs (a * point_c.x + b * point_c.y + c) / sqrt (a * a + b * b));
-//    }
-//
-//    void getLine(Point point_a, Point point_b, double &a, double &b, double &c)
-//    {
-//        // (x- p1X) / (p2X - p1X) = (y - p1Y) / (p2Y - p1Y)
-//        a = point_a.y - point_b.y; // Note: this was incorrectly "y2 - y1" in the original answer
-//        b = point_b.x - point_a.x;
-//        c = point_a.x * point_b.y - point_b.x * point_a.y;
-//    }
-//
-//    bool isSamePoint(Point a, Point b) {
-//        if (a.x == b.x && a.y == b.y)
-//            return true;
-//        return false;
-//    }
-//
-//    std::vector <Point> intersection(std::vector <Point> &v1,
-//                                           std::vector <Point> &v2) {
-//        std::vector <Point> v3;
-//
-//        std::sort(v1.begin(), v1.end());
-//        std::sort(v2.begin(), v2.end());
-//
-//        std::set_intersection(v1.begin(), v1.end(),
-//                              v2.begin(), v2.end(),
-//                              std::back_inserter(v3));
-//        return v3;
-//    }
+
+    std::vector <std::pair<Point, Point>> ransac_cal(std::vector<Point> points) {
+
+        int points_size = points.size();
+        int iterations = (int) (points_size * 0.2);
+        std::pair <Point, Point> best_candidate_pair;
+        std::vector <std::pair<Point, Point>> best_candidates;
+        double best_candidate_inliers_count = 0;
+
+        // Unless we have discarded 95% of the points keep going
+        //while (points.size() > (int) (0.05 * points_size)) {
+            // In one iteration of this loop have detected one line
+            while (iterations--) {
+                // Random number between zero and one
+                double random1 = ((double) rand() / RAND_MAX);
+                random1 *= points.size();
+                double random2 = ((double) rand() / RAND_MAX);
+                random2 *= points.size();
+
+                std::vector <Point> inliers, outliers;
+
+                Point point_a = points[(int) random1];
+                // points.erase(points.begin() + (int) random1);
+                Point point_b = points[(int) random2];
+                // points.erase(points.begin() + (int) random1);
+
+                // First - A point, Second - Distance of "First" point from the line formed by point_a & point_b
+                std::vector<std::pair<Point ,double>> distances;
+
+                for (int i = 0; i < points.size(); i++) {
+                    if (i == (int) random1 || i == (int) random2)
+                        continue;
+
+                    // Calculate distance of points from the line formed by point_a & point_b
+                    distances.push_back(calculate_dist(point_a, point_b, points[i]));
+                }
+
+                /*
+                 * Now from the distance vector populate inliers and outliers by comparing with the threshold,
+                 * if the number of outliers is max amongst the ones detected so far this is the best candidate.
+                 * */
+                int inliers_count = 0;
+                for (auto i: distances) {
+                    if (i.second < inlier_threshold) {
+                        inliers_count++;
+                        // inliers.push_back(i);
+                    } else {
+                        outliers.push_back(i.first);
+                    }
+                }
+
+                if (inliers_count > best_candidate_inliers_count) {
+                    // Discard the inliers
+                    //points = intersection(points, outliers);
+                    best_candidate_pair.first = point_a;
+                    best_candidate_pair.second = point_b;
+                    best_candidate_inliers_count = inliers.size();
+                }
+            }
+            best_candidates.push_back(best_candidate_pair);
+        //}
+        return best_candidates;
+    }
+
+    // Calculate distance of point_c from the line being formed by point_a & point_b
+    // Adapted from the formula & https:stackoverflow.com/a/12132746
+    std::pair<Point ,double> calculate_dist(Point point_a, Point point_b, Point point_c) {
+        double a, b, c;
+        getLine (point_a, point_b, a, b, c);
+        return std::pair<Point ,double>(point_c,abs (a * point_c.x + b * point_c.y + c) / sqrt (a * a + b * b));
+    }
+
+    void getLine(Point point_a, Point point_b, double &a, double &b, double &c)
+    {
+        // (x- p1X) / (p2X - p1X) = (y - p1Y) / (p2Y - p1Y)
+        a = point_a.y - point_b.y;  
+        b = point_b.x - point_a.x;
+        c = point_a.x * point_b.y - point_b.x * point_a.y;
+    }
+
+    bool isSamePoint(Point a, Point b) {
+        if (a.x == b.x && a.y == b.y)
+            return true;
+        return false;
+    }
+
+    /*std::vector <Point> intersection(std::vector <Point> &v1,
+                                           std::vector <Point> &v2) {
+        std::vector <Point> v3;
+
+        std::sort(v1.begin(), v1.end());
+        std::sort(v2.begin(), v2.end());
+
+        std::set_intersection(v1.begin(), v1.end(),
+                              v2.begin(), v2.end(),
+                              std::back_inserter(v3));
+        return v3;
+    }*/
 
 };
 
